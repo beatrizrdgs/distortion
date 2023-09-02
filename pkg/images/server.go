992725876b721f667ec28a855146df09d254ebd2
Beatriz Rodrigues <beatrmiz@icloud.com>
2023-09-02 14:10:50 -0300

    Feat: better home page

--- pkg/images/server.go
+++ pkg/images/server.go
@@ -35,21 +35,7 @@ func NewServer(s *Service) *Server {
 
 func (s *Server) showUploadForm(w http.ResponseWriter, r *http.Request) {
 
-	html := `
-    <html>
-        <body>
-            <form action="/upload" method="post" enctype="multipart/form-data">
-                <input type="file" name="image" accept="image/*">
-				<label for="transformation">Choose Transformation</label>
-				<select name="transformation" id="transformation">
-					<option value="rebaixado">Rebaixado</option>
-					<option value="pixelado">Pixelado</option>
-				</select>
-                <input type="submit" value="Upload and Transform">
-            </form>
-        </body>
-    </html>
-    `
+	html := homePage()
 	w.Write([]byte(html))
 
 }
@@ -107,4 +93,99 @@ func encodeImage(imgBuffer *bytes.Buffer, img image.Image, format string) error 
 	default:
 		return fmt.Errorf("Unsupported image format: %s", format)
 	}
+}
+
+func homePage() string {
+
+	return `
+    <html>
+        <head>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    text-align: center;
+                    background-color: #f0f0f0;
+                }
+
+                form {
+                    margin: 20px auto;
+                    padding: 20px;
+                    border: 1px solid #ccc;
+                    background-color: #fff;
+                    max-width: 400px;
+                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                }
+
+                .input-container {
+                    text-align: center;
+                }
+
+                input[type="file"] {
+                    display: none;
+                }
+
+                .file-upload-box {
+                    border: 2px dashed #ccc;
+                    padding: 20px;
+                    margin: 10px 0;
+                    cursor: pointer;
+                }
+
+                .file-upload-box:hover {
+                    border-color: #007bff;
+                }
+
+                .file-upload-box label {
+                    cursor: pointer;
+                }
+
+                select {
+                    margin: 10px 0;
+                    padding: 5px;
+                    width: 100%;
+                }
+
+                input[type="submit"] {
+                    margin: 15px 0;
+                    padding: 10px 20px;
+                    background-color: #007bff;
+                    color: #fff;
+                    border: none;
+                    cursor: pointer;
+                }
+
+                input[type="submit"]:hover {
+                    background-color: #0056b3;
+                }
+            </style>
+        </head>
+        <body>
+            <form action="/upload" method="post" enctype="multipart/form-data">
+                <div class="file-upload-box">
+                    <label for="image" id="file-label">Upload Image</label>
+                    <input type="file" name="image" id="image" accept="image/*" onchange="handleFileSelect(event)">
+                </div>
+                <br>
+                <label for="transformation">Choose Transformation:</label>
+                <select name="transformation" id="transformation">
+                    <option value="rebaixado">Rebaixado</option>
+                    <option value="pixelado">Pixelado</option>
+                </select>
+                <br>
+                <input type="submit" value="Upload and Transform">
+            </form>
+            <script>
+                function handleFileSelect(event) {
+                    const fileLabel = document.getElementById('file-label');
+                    if (event.target.files.length > 0) {
+                        fileLabel.textContent = event.target.files[0].name;
+                    } else {
+                        fileLabel.textContent = 'Drag file here or click to upload';
+                    }
+                }
+            </script>
+        </body>
+    </html>
+    `
+
 }
